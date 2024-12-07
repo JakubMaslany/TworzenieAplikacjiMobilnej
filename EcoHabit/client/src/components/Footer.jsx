c001de4a79af586104c318731c17fd307ef68402
JakubMaslany <kuba123.maslany123@gmail.com>
2024-12-07 19:46:57 +0100

    6

--- EcoHabit/client/src/components/Footer.jsx
+++ EcoHabit/client/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 // src/components/Footer.jsx
 import React from 'react';
+import './Footer.css'; // Jeśli masz plik z dodatkowymi stylami dla stopki
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p>© 2024 EcoHabit. All rights reserved.</p>
+    <footer>
+      <p>© 2024 EcoHabit. Wszelkie prawa zastrzeżone.</p>
     </footer>
   );
 };
