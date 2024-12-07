7c8cd5cd8bb27489b5ce5198b1543150626c7b96
JakubMaslany <kuba123.maslany123@gmail.com>
2024-12-07 19:33:21 +0100

    12132

--- EcoHabit/client/src/App.jsx
+++ EcoHabit/client/src/App.jsx
@@ -1,31 +1,26 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import HabitDetails from './pages/HabitDetails';
-import './App.css'; // Globalne style aplikacji
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <div className="main-container">
-        <Sidebar />
-        <div className="content">
+      <div className="app-container">
+        <Navbar />
+        <div className="main-content">
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/habits/:id" element={<HabitDetails />} />
           </Routes>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </Router>
   );
 };
